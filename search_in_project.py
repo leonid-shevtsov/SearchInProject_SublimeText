a015d64aca5338893257d513ec2bbbe402212f35
Leonid Shevtsov <leonid@shevtsov.me>
2013-07-03 15:17:57 +0300

    code cleanup

--- search_in_project.py
+++ search_in_project.py
@@ -1,15 +1,16 @@
 import sublime
 import sublime_plugin
 import os.path
+import os
+import sys
+import inspect
 
-
-import os, sys, inspect
 # realpath() with make your script run, even if you symlink it :)
-cmd_folder = os.path.realpath(os.path.abspath(os.path.split(inspect.getfile( inspect.currentframe() ))[0]))
+cmd_folder = os.path.realpath(os.path.abspath(os.path.split(inspect.getfile(inspect.currentframe()))[0]))
 if cmd_folder not in sys.path:
     sys.path.insert(0, cmd_folder)
 # use this if you want to include modules from a subforder
-cmd_subfolder = os.path.realpath(os.path.abspath(os.path.join(os.path.split(inspect.getfile( inspect.currentframe() ))[0],"subfolder")))
+cmd_subfolder = os.path.realpath(os.path.abspath(os.path.join(os.path.split(inspect.getfile(inspect.currentframe()))[0], "subfolder")))
 if cmd_subfolder not in sys.path:
     sys.path.insert(0, cmd_subfolder)
  # Info:
@@ -21,6 +22,7 @@ if cmd_subfolder not in sys.path:
 import searchengines
 
 basedir = os.getcwd()
+
 
 class SearchInProjectCommand(sublime_plugin.WindowCommand):
     def __init__(self, window):
